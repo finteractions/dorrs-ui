a067283c84fda24e032647a43a57cbb5467c46b0
David <david201192@gmail.com>
2023-10-01 11:21:38 +0300

    DF-46 Frontend. Permissions

--- wrappers/portal-access-wrapper.tsx
+++ wrappers/portal-access-wrapper.tsx
@@ -16,7 +16,7 @@ export default function portalAccessWrapper<P extends { access?: any }>(
             ...props as P,
             access,
         };
-
+        console.log(finalProps)
         return (
             <>
                 {access && access.view && (
