fa473caeb928c653884a010aa745d9a2628e646a
denis <you@example.com>
2023-10-26 10:07:29 +0100

    DF-82 Frontend. Dahsboard. Hide Symbols.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,31 +2,18 @@ import React, {ReactElement} from "react"
 import type {NextPageWithLayout} from "./_app";
 import PortalLayout from "../components/layouts/portal/portal-layout";
 import MembershipBlock from "@/components/membership-block";
-import SymbolBlock from "@/components/symbol-block";
-import {useRouter} from "next/router";
 import IndicatorBlock from "@/components/indicator-block";
 
 const Dashboard: NextPageWithLayout = () => {
-
-    const router = useRouter();
-
-    const onCallback = (symbol: string) => {
-        router.push(`/symbols/${symbol}`)
-    }
 
     return (
         <>
             <div className={'flex-panel-box'}>
                 <MembershipBlock/>
                 <IndicatorBlock/>
-                <SymbolBlock
-                    isDashboard={false}
-                    onCallback={onCallback}
-                />
+
             </div>
-
-
-            {/*<SymbolBlock isDashboard={true}/>*/}
+            
         </>
     )
 }
