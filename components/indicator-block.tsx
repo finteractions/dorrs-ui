10f733841717a1ba58f1eb6220b369d1a70d0e70
David <david201192@gmail.com>
2023-10-28 17:09:10 +0300

    DF-87 Portal. Dashboard. Add graphics

--- components/indicator-block.tsx
+++ components/indicator-block.tsx
@@ -272,11 +272,16 @@ class IndicatorBlock extends React.Component {
 
                                     <button
                                         type="button"
-                                        className='border-grey-btn ripple'
+                                        className='b-btn ripple'
                                         onClick={() => this.openModal('symbol')}
                                     >
                                         <FontAwesomeIcon className="nav-icon" icon={faPlus}/>
                                     </button>
+                                </div>
+                                <div className={'indicator__item__graph'}>
+                                    <svg viewBox="0 0 370 149" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M55.2594 48.0879L-2 79.6119V156.001H375V57.8175V19.0007C337.38 -9.74674 281.927 1.79138 248.32 30.5745L206.769 62.4877C178.955 85.651 163.662 84.8257 136.841 62.4877L118.093 48.0879C94.9436 32.5697 81.2689 32.7232 55.2594 48.0879Z" fill="#C9F7F5" stroke="#19D5B2" strokeWidth={3}/>
+                                    </svg>
                                 </div>
                             </div>
                             <div className={'indicator__item'}>
@@ -285,7 +290,6 @@ class IndicatorBlock extends React.Component {
                                         dangerouslySetInnerHTML={{__html: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="part-blue-bolder" d="M22 12C22 17.5 17.5 22 12 22C6.5 22 2 17.5 2 12C2 6.5 6.5 2 12 2C17.5 2 22 6.5 22 12ZM14.5 4.5C10.4 4.5 7 7.9 7 12C7 16.1 10.4 19.5 14.5 19.5C18.6 19.5 22 16.1 22 12C22 7.9 18.6 4.5 14.5 4.5Z" fill="#718494"/><path opacity="0.3" d="M22 12C22 16.1 18.6 19.5 14.5 19.5C10.4 19.5 7 16.1 7 12C7 7.9 10.4 4.5 14.5 4.5C18.6 4.5 22 7.9 22 12ZM12 7C9.2 7 7 9.2 7 12C7 14.8 9.2 17 12 17C14.8 17 17 14.8 17 12C17 9.2 14.8 7 12 7Z" fill="#718494"/></svg>'}}/>
                                     <div>Company Profile</div>
                                 </div>
-
                                 <div>
                                     <div>
                                         <div>{this.statisticsCompanyProfile?.total || '-'}</div>
@@ -294,11 +298,16 @@ class IndicatorBlock extends React.Component {
                                     </div>
                                     <button
                                         type="button"
-                                        className='border-grey-btn ripple'
+                                        className='b-btn ripple'
                                         onClick={() => this.openModal('symbol_list')}
                                     >
                                         <FontAwesomeIcon className="nav-icon" icon={faPlus}/>
                                     </button>
+                                </div>
+                                <div className={'indicator__item__graph'}>
+                                    <svg viewBox="0 0 371 125" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M39.0013 34.0495C21.0409 19.4709 11.1465 17.918 -6 34.0495V132H376V36.5398C364.837 26.7912 362 36.5398 346.34 36.5398C330.68 36.5398 338.5 1.5 314.123 1.5H262.474C247.448 1.5 243.5 24.9186 215.938 24.9186C188.377 24.9186 179.684 6.06497 157.13 24.9186L121.333 53.9717C96.2928 71.9373 83.7265 71.3055 63.5475 53.9717L39.0013 34.0495Z" fill="#C9F7F5" stroke="#19D5B2" strokeWidth={3}/>
+                                    </svg>
                                 </div>
                             </div>
                             <div className={'indicator__item'}>
@@ -315,11 +324,16 @@ class IndicatorBlock extends React.Component {
                                     </div>
                                     <button
                                         type="button"
-                                        className='border-grey-btn ripple'
+                                        className='b-btn ripple'
                                         onClick={() => this.openModal('last_sale')}
                                     >
                                         <FontAwesomeIcon className="nav-icon" icon={faPlus}/>
                                     </button>
+                                </div>
+                                <div className={'indicator__item__graph'}>
+                                    <svg viewBox="0 0 370 149" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M-82.832 69.854L-165 116.637V230H376V84.2931V26.6876C322.015 -15.9746 242.439 1.14838 194.212 43.8635L134.586 91.2238C94.6734 125.599 72.7276 124.374 34.2393 91.2238L7.33469 69.854C-25.8846 46.8244 -45.508 47.0523 -82.832 69.854Z" fill="#C9F7F5" stroke="#19D5B2" strokeWidth={3}/>
+                                    </svg>
                                 </div>
                             </div>
                         </div>
